504447cce127389e0f2cf83808c2ae15088c5dc1
david <david.ramboz@gmail.com>
2025-08-28 08:12:03 +0200

    public route

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 serve:
 	php -S 188.165.231.198:8000 -t public/
+
+serve-local:
+	php -S localhost:8000 -t public/
---
+++ src/Controller/PublicController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PublicController extends AbstractController
+{
+    #[Route('/public/hello', name: 'public_hello', methods: ['GET'])]
+    public function hello(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Hello, this is a public route 🚀',
+        ]);
+    }
+}
